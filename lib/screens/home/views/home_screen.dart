9e510523e8024062204e41f96579118fdb011b70
Patryk Maliszewski <p.maliszewski00@gmail.com>
2024-06-12 10:51:33 +0200

    Implement HomeScreen

---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_burgers/screens/auth/blocks/sign_in_bloc/sign_in_bloc.dart';
+import 'package:my_burgers/screens/home/blocks/get_burger_bloc/get_burger_bloc.dart';
+import 'package:my_burgers/screens/home/views/details_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Row(
+          children: [
+            Image.asset('assets/logo.png', scale: 14),
+            const SizedBox(width: 8),
+            const Text(
+              'MY BURGERS',
+              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
+          IconButton(
+              onPressed: () {
+                context.read<SignInBloc>().add(SignOutRequired());
+              },
+              icon: const Icon(CupertinoIcons.arrow_right_to_line)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocBuilder<GetBurgerBloc, GetBurgerState>(
+          builder: (context, state) {
+            if (state is GetBurgerSuccess) {
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 9 / 16),
+                itemCount: state.burgers.length,
+                itemBuilder: (context, int i) {
+                  return Material(
+                    elevation: 3,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                DetailsScreen(state.burgers[i]),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(state.burgers[i].picture),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: state.burgers[i].isVeg
+                                          ? Colors.green
+                                          : Colors.red,
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 4, horizontal: 8),
+                                    child: Text(
+                                      state.burgers[i].isVeg ? "VEG" : "NON-VEG",
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 8),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.green.withOpacity(0.2),
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 4, horizontal: 8),
+                                    child: Text(
+                                      state.burgers[i].spicy == 1
+                                          ? "🌶️ BLAND"
+                                          : state.burgers[i].spicy == 2
+                                              ? "🌶️ BALANCED"
+                                              : "🌶️ SPICY",
+                                      style: TextStyle(
+                                        color: state.burgers[i].spicy == 1
+                                            ? Colors.green
+                                            : state.burgers[i].spicy == 2
+                                                ? Colors.orange
+                                                : Colors.redAccent,
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 8,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Text(
+                              state.burgers[i].name,
+                              style: const TextStyle(
+                                  fontSize: 10, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Text(
+                              state.burgers[i].description,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey.shade500,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              maxLines: 2,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "\$${state.burgers[i].price - (state.burgers[i].price * (state.burgers[i].discount) / 100)}",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "\$${state.burgers[i].price}.00",
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey.shade500,
+                                          fontWeight: FontWeight.w700,
+                                          decoration:
+                                              TextDecoration.lineThrough),
+                                    ),
+                                  ],
+                                ),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                        CupertinoIcons.add_circled_solid))
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (state is GetBurgerLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return const Center(
+                child: Text("An error has occured..."),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
